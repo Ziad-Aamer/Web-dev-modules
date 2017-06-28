c94faaec1e224b994e583467a142ddcb4d563806
Ziad Aamer <ziadaamer9@gmail.com>
2017-06-28 18:59:00 +0200

    3rd commit

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,12 +1,12 @@
 (function SpeakHelloIIFE(window){
 
-var speakWord = "Hello";
-var helloSpeaker = new Object();
-helloSpeaker.speak = function (name) {
-  console.log(speakWord + " " + name);
-}
+	var speakWord = "Hello";
+	var helloSpeaker = new Object();
+	helloSpeaker.speak = function (name) {
+	  console.log(speakWord + " " + name);
+	}
 
-window.helloSpeaker = helloSpeaker ;
+	window.helloSpeaker = helloSpeaker ;
 
 
 
